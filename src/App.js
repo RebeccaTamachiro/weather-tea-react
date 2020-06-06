c97e4755426b6d1f7a520118129b0ece854788ab
RebeccaTamachiro <62246989+RebeccaTamachiro@users.noreply.github.com>
2020-06-06 10:02:06 +0100

    Installed Bootstrap npm package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
 import CurrentWeather from "./CurrentWeather";
 import TipCard from "./TipCard";
 import SearchEngine from "./SearchEngine";
@@ -10,40 +12,42 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="row">
-        <div className="col app-title">
-          <h1>Weather Tea</h1>
-        </div>
-      </div>
-      <div className="row main-card-wrapper mt-4">
-        <div className="main-card mt-0 mb-1 p-3">
-          <h2 className="mb-0">
-            Now in
-            <span className="location"> Lisbon</span>:
-          </h2>
-          <div className="row">
-            <div className="main-info col">
-              <CurrentWeather />
-              <p class="my-0 secondary-info">
-                Last updated:
-                <span> May 22 </span>,<span>22:05</span>
-              </p>
-            </div>
-            <div className="interactive col mt-4 mr-2">
-              <TipCard />
-              <SearchEngine />
-            </div>
-          </div>
-          <h2 className="forecast-header mt-2">
-            What to expect for the next hours:
-          </h2>
-          <div className="card-group row bg-transparent">
-            <Forecast />
+      <div className="container">
+        <div className="row">
+          <div className="col app-title">
+            <h1>Weather Tea</h1>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <Credit />
+        <div className="row main-card-wrapper mt-4">
+          <div className="main-card mt-0 mb-1 p-3">
+            <h2 className="mb-0">
+              Now in
+              <span className="location"> Lisbon</span>:
+            </h2>
+            <div className="row">
+              <div className="main-info col">
+                <CurrentWeather />
+                <p class="my-0 secondary-info">
+                  Last updated:
+                  <span> May 22</span>, <span>22:05</span>
+                </p>
+              </div>
+              <div className="interactive col mt-4 mr-2">
+                <TipCard />
+                <SearchEngine />
+              </div>
+            </div>
+            <h2 className="forecast-header mt-2">
+              What to expect for the next hours:
+            </h2>
+            <div className="card-group row bg-transparent">
+              <Forecast />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <Credit />
+        </div>
       </div>
     </div>
   );
